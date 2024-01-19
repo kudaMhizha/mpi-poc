767eabbe083bffe2781481fb024e93737603fe05
Kudzai Mabika <kudzai.mabika@sovtech.com>
2024-01-19 11:01:58 +0200

    chore(prisma): init prisma setup

---
+++ apps/mpi-poc/src/app/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  private static instance: PrismaService | undefined;
+
+  public static getInstance(): PrismaService {
+    if (!PrismaService.instance) {
+      PrismaService.instance = new PrismaService();
+    }
+    return PrismaService.instance;
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
