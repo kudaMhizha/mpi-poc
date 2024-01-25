17a129ba819bdad0e7112d506007682e9bb040b5
Kuda Mhizha <kpmhizha@gmail.com>
2024-01-25 12:24:31 +0200

    test: FE changes

--- apps/web/src/app/nx-welcome.tsx
+++ apps/web/src/app/nx-welcome.tsx
@@ -428,7 +428,7 @@ export function NxWelcome({ title }: { title: string }) {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome {title} 👋
+              Welcome {title}!! 👋
             </h1>
           </div>
 
@@ -448,7 +448,7 @@ export function NxWelcome({ title }: { title: string }) {
                     d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
                   />
                 </svg>
-                <span>You&apos;re up and running</span>
+                <span>You&apos;re up and running </span>
               </h2>
               <a href="#commands"> What&apos;s next? </a>
             </div>
